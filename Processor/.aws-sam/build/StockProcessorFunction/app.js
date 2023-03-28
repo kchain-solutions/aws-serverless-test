8135c6265055957f7ddf5640ad7ebac31434a8ba
kchain-solutions <valerio.mellini87@icloud.com>
2023-03-28 22:16:43 +0100

    appsync

--- Processor/.aws-sam/build/StockProcessorFunction/app.js
+++ Processor/.aws-sam/build/StockProcessorFunction/app.js
@@ -52,63 +52,29 @@ exports.stockHandler = async (event) => {
   )
 }
 
-const eventHandler = async (event, ddbTable) => {
-  await Promise.all(
-    event.Records.map(async (record) => {
-      try {
-        console.log('Dynamo table name: ', ddbTable);
-        console.log('Bucket name event: ', record.s3.bucket.name);
-        console.log('Bucket key event: ', record.s3.object.key);
-        const s3 = new AWS.S3();
-        const originalText = await s3.getObject({
-          Bucket: record.s3.bucket.name,
-          Key: record.s3.object.key
-        }).promise();
-        console.log("original text ", originalText);
-        const jsonData = Papa.parse(originalText.Body.toString('utf-8'), { header: true, delimiter: "," }).data;
-        console.log("JsonData length ", jsonData.length);
-        await ddbLoader(jsonData, ddbTable)
-      } catch (err) {
-        console.error(err)
-      }
-    })
-  )
-}
 
 // Load JSON data to DynamoDB table
 const ddbLoader = async (data, ddbTable) => {
   // Separate into batches for upload
   let batches = []
   const BATCH_SIZE = 25
-
-
   while (data.length > 0) {
     batches.push(data.splice(0, BATCH_SIZE))
   }
-
   console.log(`Total batches: ${batches.length}`)
-
   let batchCount = 0
 
-  // Save each batch
   await Promise.all(
-
     batches.map(async (item_data) => {
-
-      // Set up the params object for the DDB call
       const params = {
         RequestItems: {}
       }
       params.RequestItems[ddbTable] = []
-
       item_data.forEach(item => {
         for (let key of Object.keys(item)) {
-          // An AttributeValue may not contain an empty string
           if (item[key] === '')
             delete item[key]
         }
-
-        // Build params
         params.RequestItems[ddbTable].push({
           PutRequest: {
             Item: {
@@ -118,7 +84,6 @@ const ddbLoader = async (data, ddbTable) => {
           }
         })
       })
-      // Push to DynamoDB in batches
       try {
         batchCount++
         console.log('Trying batch: ', batchCount)
--- README.md
+++ README.md
@@ -9,3 +9,6 @@ DOCKER_HOST=unix://$HOME/.docker/run/docker.sock sam local invoke -e ./lambda/la
 DOCKER_HOST=unix://$HOME/.docker/run/docker.sock sam local start-lambda
 
  sam local generate-event s3 put --bucket product.stock.test --key products.csv --region eu-west-3> lambda_event.json 
+
+
+sam deploy --guided --capabilities CAPABILITY_NAMED_IAM
